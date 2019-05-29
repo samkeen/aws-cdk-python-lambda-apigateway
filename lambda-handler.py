88a96c931b11714489eed5a31e5a39530c5db383
Sam Keen <samkeen@amazon.com>
2019-05-29 13:21:11 -0700

    cleanup

--- lambda-handler.py
+++ lambda-handler.py
@@ -1,28 +1,17 @@
 import json
-import boto3
 import os
+
+import boto3
+
 
 def main(event, context):
     queue_url = os.environ.get('QUEUE_URL')
-    # const sqs = new SQS();
-    # const response = await sqs.getQueueAttributes({
-    #     QueueUrl: process.env.QUEUE_URL,
-    #     AttributeNames: ['All']
-    # }).promise();
-    #
-    # return {
-    #     statusCode: 200,
-    #     body: JSON.stringify(response.Attributes, undefined, 2),
-    #     headers: {
-    #         'content-type': 'application/json'
-    #     }
-    # };
+
     sqs = boto3.client('sqs')
     queue_resp = sqs.get_queue_attributes(
         QueueUrl=queue_url,
         AttributeNames=['All']
     )
-
 
     response = {
         "statusCode": 200,
--- queue_viewer/queue_viewer_stack.py
+++ queue_viewer/queue_viewer_stack.py
@@ -10,23 +10,7 @@ class QueueViewerStack(cdk.Stack):
         # Create the SQS queue
         queue = aws_sqs.Queue(self, 'SimpleQueue')
 
-
         # Create our Lambda function
-        # lambda role
-        # const role = new iam.Role(this, 'LambdaExecutionRole', {
-        #     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
-        # });
-        #
-        # role.addToPolicy(new iam.PolicyStatement()
-        #     .addAction("dynamoDB:*")
-        #     .addResource(table.tableArn));
-        # role = aws_iam.Role(
-        #     self,
-        #     'LambdaExecutionRole',
-        #     assumed_by=aws_iam.ServicePrincipal('lambda.amazonaws.com')
-        # )
-        # role.add_to_policy(PolicyStatement().add_action('sqs:*').add_resource(queue.queue_arn))
-
         with open("lambda-handler.py", encoding="utf8") as fp:
             handler_code = fp.read()
         lambda_fn = lambda_.Function(
@@ -39,15 +23,7 @@ class QueueViewerStack(cdk.Stack):
             environment={'QUEUE_URL': queue.queue_url},
         )
         lambda_fn.add_to_role_policy(PolicyStatement().add_action('sqs:*').add_resource(queue.queue_arn))
-        # statement = PolicyStatement()
-        # statement.addServicePrincipal('cloudwatch.amazonaws.com')
-        # statement.addServicePrincipal('lambda.amazonaws.com')
-        # statement.addAwsPrincipal('arn:aws:boom:boom')
-        # lambda_fn.add_to_role_policy(statement)
-        # queue.grant(lambda_fn.role, 'sqs:GetQueueAttributes')
-
         # Now create our API for the Lambda function
-
         api = aws_apigateway.LambdaRestApi(
             self,
             'QueueViewerApi',
